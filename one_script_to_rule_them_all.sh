8bbc4daf2dcd8e04ef044406b774942dd7ecee83
Zonily Jame <kuyazee@users.noreply.github.com>
2019-07-31 11:22:50 +0800

    Update one_script_to_rule_them_all.sh

--- one_script_to_rule_them_all.sh
+++ one_script_to_rule_them_all.sh
@@ -13,4 +13,61 @@ sudo gem install cocoapods
 brew update
 
 # Install Apps
-brew bundle Brewfile
+brew install python3
+brew install node
+brew install openssl
+brew install docker
+brew install sqlite
+brew install vapor/tap/vapor
+brew install swiftlint
+brew install swiftformat
+brew install sourcedocs
+brew install postgres
+brew install cocoapods
+brew install zsh
+brew install youtube-dl
+brew install wget
+brew install mxcl/made/swift-sh
+
+# Work
+brew cask install android-studio
+brew cask install appcode
+brew cask install sublime-text
+brew cask install visual-studio-code
+brew cask install iterm2
+brew cask install sequel-pro
+brew cask install sourcetree
+brew cask install fastlane
+
+# Mac Experience
+brew cask install spectacle
+brew cask install karabiner
+brew cask install alfred
+brew cask install monolingual
+
+# Quicklook
+brew cask install suspicious-package
+brew cask install quicklook-json
+brew cask install provisionql
+brew cask install qlplayground
+brew cask install quicklookase
+brew cask install qlvideo
+brew cask install quicklookapk
+brew cask install webpquicklook
+brew cask install qlstephen
+
+# Apps
+brew cask install lastpass
+brew cask install google-chrome
+brew cask install slack
+brew cask install macdown
+brew cask install telegram
+brew cask install franz
+brew cask install discord
+
+# Personal Apps
+brew cask install steam
+brew cask install spotify
+brew cask install vlc
+brew cask install iina
+brew cask install youtube-to-mp3
