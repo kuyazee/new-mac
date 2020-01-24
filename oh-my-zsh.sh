ee29799228cc38882d6134d839fff4df2ec93323
Zonily Jame Pesquera <zonilyjame@gmail.com>
2020-01-24 10:30:26 +0800

    Added comments and now installing powerline fonts

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,3 +1,12 @@
 brew install zsh zsh-completions
+
+# Install robbyrussel zsh theme
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install powerline fonts
+git clone https://github.com/powerline/fonts.git
+source ./fonts/install.sh
+rm -rf ./fonts
